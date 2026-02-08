ba614129ad65d8241c9f776826d6dfdbc1fa53c9
Albera0 <yangxiaolong724@gmail.com>
2026-02-08 23:51:46 +0100

    分布式锁代码开发

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,14 +1,19 @@
 package com.hmdp;
 
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -21,6 +26,34 @@ class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es =  Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            countDownLatch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+
+        System.out.println("time = " + (end - begin));
+    }
 
     @Test
     void testSaveShop() {
